47c55d19fc8341c56513eb577a1ef8008d900a44
MariusDevelops <brazasmarius@gmail.com>
2023-04-12 14:00:16 +0300

    init: server connected with mongoDB

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB Connected'))
+  .catch((err) => console.log(err));
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
