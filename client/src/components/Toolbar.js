72fdf816392a36dc12afb14faab989dab88a2039
MariusDevelops <brazasmarius@gmail.com>
2023-04-13 11:38:31 +0300

    feat: logout implemented

--- client/src/components/Toolbar.js
+++ client/src/components/Toolbar.js
@@ -1,29 +1,16 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-
-// const Toolbar = () => {
-//   return (
-//     <div className="navigation-bar">
-//       <div>
-//         <Link to="/">Home</Link>
-//         <Link to="/register">Register</Link>
-//         <Link to="/login">Login</Link>
-//         <Link to="/profile">Profile</Link>
-//         <Link to="/allusers">All Users</Link>
-//         <Link to="/conversations">Conversations</Link>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Toolbar;
-
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { clearUser } from '../features/dataSlice';
 
 const Toolbar = () => {
   const isLoggedIn = useSelector((state) => state.data.isLoggedIn);
+
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(clearUser());
+  };
 
   return (
     <div className="navigation-bar">
@@ -32,10 +19,12 @@ const Toolbar = () => {
         <Link to="/register">Register</Link>
         {isLoggedIn && (
           <>
-            <Link to="/login">Login</Link>
             <Link to="/profile">Profile</Link>
             <Link to="/allusers">All Users</Link>
             <Link to="/conversations">Conversations</Link>
+            <Link to="/" onClick={handleLogout}>
+              Logout
+            </Link>
           </>
         )}
         {!isLoggedIn && <Link to="/login">Login</Link>}
